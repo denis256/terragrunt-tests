37a684b8447fea5ea98c0adb7e608c455e279b13
Denis O <denis.o@linux.com>
2023-04-21 20:58:47 +0000

    init testing

---
+++ init-error/dependency-init/app/main.tf
@@ -0,0 +1,16 @@
+#provider "aws" {
+#  region  = "us-east-12"
+#  profile = "test-profile"
+#}
+#
+#
+#terraform {
+#  backend "s3" {
+#    encrypt = true
+#    bucket = "test-bucket"
+#    dynamodb_table = "test-ddb"
+#    region = "us-east-1"
+#    key = "terraform.tfstate"
+#  }
+#}
+#
