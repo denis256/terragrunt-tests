3b26b228c852380955f763db444301f735df8822
Denis O <denis.o@linux.com>
2023-11-18 05:58:26 -0800

    add variable definition

--- boilerplate1/variables.tf
+++ boilerplate1/variables.tf
@@ -1,6 +1,10 @@
 variable "project_name" {}
-variable "replica_count" {}
+variable "replica_count" {
+  description = "Description of replica_count"
+
+}
 
 variable "d1" {
+  description = "D1 description"
   default = "default1"
 }
