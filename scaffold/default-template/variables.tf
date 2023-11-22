e482a0a40ebc153cc82a764f85461abf6330339a
Denis O <denis.o@linux.com>
2023-11-22 11:53:53 -0800

    updated test3

--- scaffold/default-template/variables.tf
+++ scaffold/default-template/variables.tf
@@ -7,9 +7,13 @@ variable "project_name" {
 variable "test_1" {}
 
 variable "test_2" {
-
   default = {
     x = 1
   }
+}
 
+variable "test_3" {
+  type = integer
+  default = 666
+  description = "description test 3"
 }
