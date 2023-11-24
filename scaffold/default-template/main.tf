00a81d3f1277653a129a5d3d2289bdd75a548de6
Denis O <denis.o@linux.com>
2023-11-24 10:53:00 +0200

    config file update

--- scaffold/default-template/main.tf
+++ scaffold/default-template/main.tf
@@ -18,5 +18,5 @@ variable "enabled" {
 
 resource "local_file" "config" {
   content  = "${var.project_name} vpc: ${var.vpc} replica_count: ${var.replica_count}"
-  filename = "${path.module}/conifg.txt"
+  filename = "conifg.txt"
 }
