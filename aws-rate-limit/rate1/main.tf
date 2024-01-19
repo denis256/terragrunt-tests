cc244f1f8702bfdc8c75b04ed50d30fdea443c16
Denis O <denis.o@linux.com>
2024-01-19 17:15:15 +0000

    api test

--- aws-rate-limit/rate1/main.tf
+++ aws-rate-limit/rate1/main.tf
@@ -1,10 +1,32 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
 }
 
 # Example list of bucket names - replace with actual bucket names in your account
 variable "bucket_names" {
-  default = ["denis-test-us-east-2", "denis-test-us-east-2-2", "denis-test-us-east-2-3"] # Add as many bucket names as possible
+  default = [
+    "test-denis-tf-2024-1",
+    "test-denis-tf-2024-10",
+    "test-denis-tf-2024-11",
+    "test-denis-tf-2024-12",
+    "test-denis-tf-2024-13",
+    "test-denis-tf-2024-14",
+    "test-denis-tf-2024-15",
+    "test-denis-tf-2024-16",
+    "test-denis-tf-2024-17",
+    "test-denis-tf-2024-18",
+    "test-denis-tf-2024-19",
+    "test-denis-tf-2024-2",
+    "test-denis-tf-2024-20",
+    "test-denis-tf-2024-21",
+    "test-denis-tf-2024-3",
+    "test-denis-tf-2024-30",
+    "test-denis-tf-2024-4",
+    "test-denis-tf-2024-5",
+    "test-denis-tf-2024-6",
+    "test-denis-tf-2024-7",
+    "test-denis-tf-2024-8"
+  ]
 }
 
 # Data source to get information on each S3 bucket
