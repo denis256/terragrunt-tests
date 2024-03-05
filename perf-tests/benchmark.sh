26fb077a61c84b4565ed8c1d3d126f8c8ea30ef8
Denis O <denis.o@linux.com>
2024-03-05 15:51:18 +0000

    benchmark code update

--- perf-tests/benchmark.sh
+++ perf-tests/benchmark.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
+set -x
 source ./config.sh
 
 COMMAND="terragrunt graph-dependencies"
 
-for TG_VER in "${TG_VERS[@]}"; do
+for TG_VER in "${versions[@]}"; do
     echo Run Terragrunt version ${TG_VER}
-    $c run --rm -v "$(pwd)/code:/code" -v "$(pwd):/output" terragrunt-benchmark:${TG_VER} /usr/bin/time -ao /output/result.tg${TG_VER}.txt ${COMMAND}
+    docker run --rm -v "$(pwd)/code:/code" -v "$(pwd):/output" terragrunt-benchmark:${TG_VER} /usr/bin/time -ao /output/result.tg${TG_VER}.txt ${COMMAND}
 done
