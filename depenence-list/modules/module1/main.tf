0053094ddda28ed370194b61551482456db321e5
Denis O <denis.o@linux.com>
2024-10-28 19:47:56 +0000

    add test inputs

--- depenence-list/modules/module1/main.tf
+++ depenence-list/modules/module1/main.tf
@@ -2,3 +2,7 @@ resource "local_file" "module1" {
     content     = "module1"
     filename = "${path.module}/module1.txt"
 }
+
+output "m1" {
+    value = "m1"
+}
--- depenence-list/modules/module2/main.tf
+++ depenence-list/modules/module2/main.tf
@@ -2,3 +2,7 @@ resource "local_file" "module2" {
     content     = "module2"
     filename = "${path.module}/module2.txt"
 }
+
+output "m2" {
+    value = "m2"
+}
