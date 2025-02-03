92abbce59cdcb025e786b70b5d0a8bf9a3f038a7
Denis O <denis.o@linux.com>
2025-02-03 20:32:24 +0000

    updated test code

--- stacks/outputs/units/aapl/main.tf
+++ stacks/outputs/units/aapl/main.tf
@@ -6,6 +6,17 @@ output "ytd" {
   value = 4.01
 }
 
+output "provider" {
+  value = {
+    "name" = "yahoo"
+    "url" = "https://finance.yahoo.com/quote/AAPL"
+    timetamp = timestamp()
+    id = 666
+    delta = 0.01
+  }
+}
+
+
 resource "local_file" "config" {
   content     = " file"
   filename = "${path.module}/config.txt"
--- stacks/outputs/units/nvda/main.tf
+++ stacks/outputs/units/nvda/main.tf
@@ -10,6 +10,16 @@ output "price" {
   value = -666
 }
 
+output "provider" {
+  value = {
+    "name" = "yahoo"
+    "url" = "https://finance.yahoo.com/quote/NVDA"
+    timetamp = timestamp()
+    id = 777
+    delta = 0.02
+  }
+}
+
 resource "local_file" "config" {
   content     = " file"
   filename = "${path.module}/config.txt"
