4892ab8a5bad89f70e9167db791d81aec1440b6c
Denis O <denis.o@linux.com>
2025-04-07 20:32:58 +0100

    example update

--- project-backend/main.tf
+++ project-backend/main.tf
@@ -2,3 +2,19 @@
 output "data" {
   value = "backend"
 }
+
+output "price" {
+  value = 123
+}
+
+output "enabled" {
+  value = true
+}
+
+output "map" {
+  value = {
+    "key1" = "value1"
+    "key2" = 123
+    "key3" = true
+  }
+}
--- stacks-test/nested-output/units/dev-unit/main.tf
+++ stacks-test/nested-output/units/dev-unit/main.tf
@@ -1,3 +1,19 @@
 output "data" {
   value = "dev-unit"
 }
+
+output "price" {
+  value = 123
+}
+
+output "enabled" {
+  value = true
+}
+
+output "map" {
+  value = {
+    "key1" = "value1"
+    "key2" = 123
+    "key3" = true
+  }
+}
