1b58c640c789aa9fe15b507ee4832636e62d8b58
Denis O <denis.o@linux.com>
2025-07-01 15:31:31 +0100

    basic tests update

---
+++ stacks-test/basic/units/chicken/main.tf
@@ -0,0 +1,5 @@
+# create local file
+resource "local_file" "test" {
+  content  = "Hello, World!"
+  filename = "${path.module}/test.txt"
+}
--- stacks-test/basic/units/father/main.tf
+++ stacks-test/basic/units/father/main.tf
@@ -2,3 +2,10 @@ resource "local_file" "file" {
   content     = " file"
   filename = "${path.module}/cluster_name.txt"
 }
+
+
+# create local file
+resource "local_file" "test" {
+  content  = "Hello, World!"
+  filename = "${path.module}/test.txt"
+}
